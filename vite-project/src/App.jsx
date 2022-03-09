9e08df6b7248cb7af8204edbf079a2b9660d5f73
gribdesbois <gribdesbois@gmail.com>
2022-03-09 09:05:45 +0100

    getCurrentPosition

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,8 +7,30 @@ function App() {
   const [lat, setLat] = useState([])
   const [long, setLong] = useState([])
 
-  useEffect(() => {})
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+    function error(err) {
+      if (err) {
+        console.warn(`ERROR(${err?.code}): ${err?.message}`)
+      }
+      return
+    }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+      },
+      error(),
+      options
+    )
+  }, [lat, long])
 
+  console.log('Latitude is:', lat)
+  console.log('Longitude is:', long)
   return <div className="App"></div>
 }
 
