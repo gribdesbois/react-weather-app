39cc40b3a85c5ec5f906195e0a79c900e215fb76
gribdesbois <gribdesbois@gmail.com>
2022-03-09 08:39:50 +0100

    initial Setup + geolocation

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+const { VITE_API_KEY } = import.meta.env
+
+function App() {
+  const [lat, setLat] = useState([])
+  const [long, setLong] = useState([])
+
+  useEffect(() => {})
+
+  return <div className="App"></div>
+}
+
+export default App
